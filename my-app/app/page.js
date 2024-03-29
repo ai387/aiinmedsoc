c4f8037a556666cc568a76089ea716fb89d4a704
Arzina <arzina@arzinas-mbp.home>
2024-03-29 23:43:03 +0000

    more edits to homepage

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
 
     //<main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <main className="flex min-h-screen items-center justify-between p-24">
+    <main className="flex min-h-screen items-center justify-center p-24">
       
       <div className="grid grid-cols-5 absolute inset-x-0 top-0 justify-center p-24">
       
@@ -19,7 +19,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-1xl font-semibold`}>
-              About Us{" "}
+              About {" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
@@ -84,7 +84,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2 className={`mb-3 text-1xl font-semibold`}>
-              Contact{" "}
+              Contact {" "}
               <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                 -&gt;
               </span>
@@ -93,13 +93,16 @@ export default function Home() {
         </div>
 
       </div>
+            
+      <div className="text-2xl font-semibold items-center
+      before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] 
+      after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] 
+      before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10
+      after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div>
+          Cambridge University Medicine in AI Society <br/>
+        </div>
 
-
-
-      <div className="mb-3 text-2xl text-black font-semibold relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <p>
-          Cambridge University Medicine in AI Society
-        </p>
       </div>
 
 
